1024798bed048aa9fb0ab95e41eeeb3762c5d165
Grace Tian <gracetian4ever@gmail.com>
2020-06-04 19:17:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
- line_friends = ["brown", "sally","tata","rj"]
+ line_friends = ["brown", "sally", "tata", "rj"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  line_friends.unshift()
+  line_friends.unshift("koya")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  line_friends.pop()
 end
 
 def remove_element_from_start_of_array(array)
