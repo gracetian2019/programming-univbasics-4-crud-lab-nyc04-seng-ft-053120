b4515df6871b7680cf4ee01387f893726febfdc8
Grace Tian <gracetian4ever@gmail.com>
2020-06-04 19:17:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  line_friends.push("")
+  line_friends.push("shooky")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  line_friends.unshift()
 end
 
 def remove_element_from_end_of_array(array)
